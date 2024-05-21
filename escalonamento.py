2455ca8ced7c17654dc7a0e71b4701986b12f8a4
Lincon Brito <linconjbrito@gmail.com>
2024-05-20 21:37:57 -0300

    sjf adicionado e funcionando, OK grupogit checkout sjf

--- escalonamento.py
+++ escalonamento.py
@@ -22,6 +22,7 @@ def execucao():
                      
                     case 2:
                         print("SJF. Vamos lá!")
+                        sjf()
 
                     case 3:
                         print("Round Robin. Vamos lá!")
@@ -58,6 +59,50 @@ def fifo():
             print(f"Turnaround: {v['T_medio']:.2f}")
         else:
             print(f"{k+1} -- > {v['T_medio']}")
+
+
+
+
+
+
+def sjf():
+    # Inicialização de variáveis
+    tempo_atual = 0
+    lista_final = []
+    turnaround = 0
+
+    # Lista de processos ordenados por tempo de chegada
+    lista_processos2 = sorted(lista_processos, key=lambda dicionario: dicionario["T_chegada"])
+
+    # Enquanto houver processos na lista de processos
+    while lista_processos2:
+        # Filtra processos que já chegaram
+        processos_disponiveis = [p for p in lista_processos2 if p["T_chegada"] <= tempo_atual]
+        
+        if processos_disponiveis:
+            # Seleciona o processo com o menor tempo de execução
+            proximo_processo = min(processos_disponiveis, key=lambda p: p["T_exec"])
+            lista_processos2.remove(proximo_processo)
+            
+            # Calcula o tempo de espera e o tempo médio
+            espera = max(0, tempo_atual - proximo_processo["T_chegada"])
+            proximo_processo["T_medio"] = espera + proximo_processo["T_exec"]
+            turnaround += proximo_processo["T_medio"]
+            
+            # Atualiza o tempo atual
+            tempo_atual += proximo_processo["T_exec"]
+            lista_final.append(proximo_processo)
+        else:
+            # Se não há processos disponíveis, avança o tempo
+            tempo_atual = lista_processos2[0]["T_chegada"]
+
+    # Calcula o turnaround médio
+    turnaround_medio = turnaround / qnt_processos
+    lista_final.append({"T_medio": turnaround_medio})
+
+    # Imprime o resultado
+    for processo in lista_final:
+        print(f"{processo}\n")
 
 
 
